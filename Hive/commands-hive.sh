9195a440f7be25b9752bfb985a8dea0014947d81
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2025-04-12 15:44:51 +0530

    Update project directory structure

--- Hive/commands-hive.sh
+++ Hive/commands-hive.sh
@@ -4,11 +4,11 @@ start-yarn.sh
 hdfs dfs -mkdir -p /user/hive/warehouse/mydata
 hdfs dfs -put ../cleaned_courses.csv /user/hive/warehouse/mydata/
 hdfs dfs -put ../Enrollment_Data.csv /user/hive/warehouse/mydata/
-hdfs dfs -put G../radeRosterReport.csv /user/hive/warehouse/mydata/
+hdfs dfs -put ../GradeRosterReport.csv /user/hive/warehouse/mydata/
 
-beeline -u jdbc:hive2:// -f ./delete_tables.hql
-beeline -u jdbc:hive2:// -f ./setup_hive_tables.hql
-beeline -u jdbc:hive2:// -f ./warehouse_creation.hql
-beeline -u jdbc:hive2:// -f ./warehouse_creation_optimized.hql
+beeline -u jdbc:hive2:// -f ./Scripts/delete_tables.hql
+beeline -u jdbc:hive2:// -f ./Scripts/setup_hive_tables.hql
+beeline -u jdbc:hive2:// -f ./Scripts/warehouse_creation.hql
+beeline -u jdbc:hive2:// -f ./Scripts/warehouse_creation_optimized.hql
 
 beeline -u jdbc:hive2:// --outputformat=tsv2 -e "SELECT * FROM dw_student_course_summary" > dw_student_course_summary.tsv
